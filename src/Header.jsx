373cc38ebed30812832a3a3e812702666090e066
Soroush Kami <soroushkami10@gmail.com>
2024-09-14 23:45:52 -0400

    update Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,14 @@
     return(
         <header>
             <h1>my react app</h1>
+            <nav>
+                <ul>
+                        <li>home</li>
+                    <li>about</li>
+                    <li><a href="">info</a></li>
+                </ul>
+            </nav>
+            <hr />
         </header>
     );
  }
