5ea4508558d782e3cf1e63eb0a2aa434204ed8b6
Soroush Kami <soroushkami10@gmail.com>
2024-09-15 11:48:57 -0400

    Cleanup files

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import Student from "./Student.jsx"
 function App() {
     return(
         <>
-            <Student name="sam" age= {30} isStudent= {true} ></Student>
-            <Student name="Sara" age={25} isStudent= {false} />
-            <Student/>
-
         </>
     );
 }
