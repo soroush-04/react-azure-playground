f91eee4ab9be8d166a308b35f1cafe1bd20d3e7b
Soroush Kami <soroushkami10@gmail.com>
2024-09-20 01:41:21 -0400

    Functionality of the todolist

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -9,19 +9,33 @@ function ToDoList() {
     }
 
     function addTask() {
-
+        if(newTask.trim() !== "") {
+            setTasks(t => [...t, newTask]);
+            setNewTask("")
+        }
     }
 
     function deleteTask(index) {
-
+        const updatedTask = tasks.filter((_, i) => index !== i);
+        setTasks(updatedTask);
     }
 
     function moveTaskUp(index) {
-
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = 
+            [updatedTasks[index-1], updatedTasks[index]];
+            setTasks(updatedTasks)
+        }
     }
 
     function moveTaskDown(index) {
-
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index+1]] = 
+            [updatedTasks[index+1], updatedTasks[index]];
+            setTasks(updatedTasks)
+        }
     }
 
     return(
