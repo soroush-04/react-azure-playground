eb2f2b3d8b59a1052b146a57f186bd9591c87b73
Soroush Kami <soroushkami10@gmail.com>
2024-09-23 17:03:20 -0400

    useEffect with dependencies

--- src/Component.jsx
+++ src/Component.jsx
@@ -2,18 +2,30 @@ import React, {useState, useEffect}  from "react";
 
 function Component() {
     const [count, setCount] = useState(0);
+    const [color, setColor] = useState('Green');
 
     useEffect(() => {
-        document.title = `count ${count}`;
-    });
+        document.title = `count ${count} ${color}`;
+    }, [count]);
 
     function addCount() {
         setCount(c => c+1);
     }
 
+    function subtractCount() {
+        setCount(c => c-1);
+    }
+
+    function changeColor() {
+        setColor(c => c === "Green" ? "Red" : "Green");
+    }
+
     return(<>
-        <p>Count: {count}</p>
+        <p style={{color: color}}>Count: {count}</p>
         <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button>
+        <br />
+        <button onClick={changeColor}>change color</button>
     </>);
 }
 
